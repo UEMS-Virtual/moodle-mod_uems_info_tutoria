f572c8bc24e5e5aa4b3d64c1d60b483c62fce7b0
BrenoAugustoUEMS <brenoaugusto93@gmail.com>
2026-05-17 10:42:06 -0400

    Align release label with Moodle branch version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_uemsinfotutoria';
-$plugin->version = 2026051604;
+$plugin->version = 2026051605;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.0';
+$plugin->release = '4.5.1';
